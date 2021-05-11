31246059b1b06f176de55f47c9237b15b906422e
Remi shan <cherukattilremi@gmail.com>
2021-05-11 20:05:05 +0530

    Add project files.

---
+++ KeyBoardMangement/Key.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace KeyBoardMangement
+{
+    partial class Key
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMove
+            // 
+            this.btnMove.BackColor = System.Drawing.Color.Red;
+            this.btnMove.Location = new System.Drawing.Point(177, 160);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 0;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = false;
+            this.btnMove.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnMove_KeyDown);
+            this.btnMove.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnMove_MouseClick);
+            // 
+            // Key
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(483, 385);
+            this.Controls.Add(this.btnMove);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Key";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Key";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMove;
+    }
+}
+
