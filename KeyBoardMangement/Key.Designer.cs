5423c9f2268fad44f64e6db39aae2cb1c033b972
Remi shan <cherukattilremi@gmail.com>
2021-05-12 14:01:27 +0530

    code changes

--- KeyBoardMangement/Key.Designer.cs
+++ KeyBoardMangement/Key.Designer.cs
@@ -29,38 +29,52 @@
         private void InitializeComponent()
         {
             this.btnMove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnMove
             // 
-            this.btnMove.BackColor = System.Drawing.Color.Red;
-            this.btnMove.Location = new System.Drawing.Point(177, 160);
+            this.btnMove.BackColor = System.Drawing.Color.DimGray;
+            this.btnMove.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMove.Location = new System.Drawing.Point(200, 157);
             this.btnMove.Name = "btnMove";
-            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.Size = new System.Drawing.Size(100, 55);
             this.btnMove.TabIndex = 0;
             this.btnMove.Text = "Move";
             this.btnMove.UseVisualStyleBackColor = false;
             this.btnMove.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnMove_KeyDown);
             this.btnMove.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnMove_MouseClick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
             // Key
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(483, 385);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnMove);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Key";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Key";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- KeyBoardMangement/Key.cs
+++ KeyBoardMangement/Key.cs
@@ -24,18 +24,29 @@ namespace KeyBoardMangement
 
         private void btnMove_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode==Keys.A)
+           // label1.Text = "Please enter A  in Keyboard";
+            if (e.KeyCode==Keys.A && btnMove.Left>=0)
             {
-                btnMove.Left = 1;
+                btnMove.Left = btnMove.Left-10;
             }
-            else if (e.KeyCode==Keys.H)
+          //  label1.Text = "Please enter T in Keyboard";
+            else if (e.KeyCode==Keys.T&& btnMove.Top>=0)
             {
-                btnMove.Top = 1;
+                btnMove.Top = btnMove.Top-10;
             }
-            else if (e.KeyCode==Keys.C)
+            else if (e.KeyCode==Keys.D && btnMove.Left > 0)
             {
-               // btnMove= 1;
+                btnMove.Left = btnMove.Left + 10; ;
             }
+            else if (e.KeyCode == Keys.B&& btnMove.Top >= 0)
+            {
+                btnMove.Top = btnMove.Top+ 10; ;
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+            
         }
     }
 }
